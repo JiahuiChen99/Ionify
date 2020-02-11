db35d6a1c3d2fce21c84632792bdeab413042533
Jiahu <Jiahui1@hotmail.es>
2020-02-11 09:23:39 +0100

    feat: Ionic App routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,6 +4,14 @@ import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 const routes: Routes = [
   { path: '', redirectTo: 'home', pathMatch: 'full' },
   { path: 'home', loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)},
+  {
+    path: 'music-list',
+    loadChildren: () => import('./pages/music-list/music-list.module').then( m => m.MusicListPageModule)
+  },
+  {
+    path: 'music-details',
+    loadChildren: () => import('./pages/music-details/music-details.module').then( m => m.MusicDetailsPageModule)
+  },
 ];
 
 @NgModule({
