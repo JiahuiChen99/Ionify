2737c0b5ad08376b17b3face7159d451b6660efd
MasterofDragons <Jiahui1@hotmail.es>
2020-02-11 23:15:15 +0100

    feat: Displaying the data

--- src/app/pages/music-details/music-details.page.ts
+++ src/app/pages/music-details/music-details.page.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { SallefyAPIService } from 'src/app/services/sallefy-api.service';
 
 @Component({
   selector: 'app-music-details',
@@ -7,9 +9,29 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MusicDetailsPage implements OnInit {
 
-  constructor() { }
+  information: any;
+
+  trackInfo: any;
+
+  constructor(private activatedRoute: ActivatedRoute, private service: SallefyAPIService) { }
 
   ngOnInit() {
+    let songName = this.activatedRoute.snapshot.paramMap.get('id');
+ 
+    // Get the information from the API
+    this.service.retrieveSpecificTrack(songName).subscribe(result => {
+      this.information = result;
+      
+    
+      this.trackInfo = this.information.tracks;
+      console.log(this.trackInfo)
+      
+    });
+
+    
   }
 
+  openWebsite() {
+    window.open(this.information.Website, '_blank');
+  }
 }
