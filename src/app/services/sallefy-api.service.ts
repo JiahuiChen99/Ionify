c08f4dba68aec26e4eb2f28b398f4fdc2781fb48
Jiahu <Jiahui1@hotmail.es>
2020-02-12 12:07:52 +0100

    chore: API_key changed

--- src/app/services/sallefy-api.service.ts
+++ src/app/services/sallefy-api.service.ts
@@ -9,7 +9,7 @@ export class SallefyAPIService {
 
   url = 'http://sallefy.eu-west-3.elasticbeanstalk.com/api/';
   // tslint:disable-next-line: max-line-length
-  apiKey = 'eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJhZG1pbiIsImF1dGgiOiJST0xFX0FETUlOLFJPTEVfVVNFUiIsImV4cCI6MTU4MTQ5Nzc1MH0.84HZwmTHSalQSzigZbuDkdxOFlbxcEPg4sktMrMiR0wVsfuFOAQj9lF1cncX3UoyatCUjRm_Kxih-13FvRUDYg';
+  apiKey = 'eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJ1c2VyIiwiYXV0aCI6IlJPTEVfVVNFUiIsImV4cCI6MTU4MTU4NzEwMH0.J-tSMLfeBgmtmJu7TFxJNK1O0uzMDqfoHuDdHhP0DndA10Z9nGbDcT1IyWlgLb5lTgaj-YmHe8veOrWGCuDm-Q';
   
   
 
