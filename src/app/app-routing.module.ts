78ad31f050ac3e7c840b5f7253df49e4b04f59a5
Jiahu <Jiahui1@hotmail.es>
2020-02-17 11:03:31 +0100

    chroe: Routing Updated

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -10,7 +10,15 @@ const routes: Routes = [
   {
     path: 'music-details/:id',
     loadChildren: () => import('./pages/music-details/music-details.module').then( m => m.MusicDetailsPageModule)
+  },  {
+    path: 'tabs',
+    loadChildren: () => import('./pages/tabs/tabs.module').then( m => m.TabsPageModule)
   },
+  {
+    path: 'profile',
+    loadChildren: () => import('./pages/profile/profile.module').then( m => m.ProfilePageModule)
+  },
+
 ];
 
 @NgModule({
