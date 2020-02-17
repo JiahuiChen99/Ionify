24ec723a6924618ce413423addafd74110441f12
Jiahu <Jiahui1@hotmail.es>
2020-02-17 11:49:29 +0100

    feat: Tab Routing Set

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,23 +2,16 @@ import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
-  { path: '', redirectTo: 'music-list', pathMatch: 'full' },
+  { path: '', redirectTo: 'tabs', pathMatch: 'full' },
   {
-    path: 'music-list',
-    loadChildren: () => import('./pages/music-list/music-list.module').then( m => m.MusicListPageModule)
-  },
-  {
-    path: 'music-details/:id',
-    loadChildren: () => import('./pages/music-details/music-details.module').then( m => m.MusicDetailsPageModule)
-  },  {
     path: 'tabs',
     loadChildren: () => import('./pages/tabs/tabs.module').then( m => m.TabsPageModule)
   },
   {
-    path: 'profile',
-    loadChildren: () => import('./pages/profile/profile.module').then( m => m.ProfilePageModule)
+    path: 'music-details/:id',
+    loadChildren: () => import('./pages/music-details/music-details.module').then( m => m.MusicDetailsPageModule)
   },
-
+  
 ];
 
 @NgModule({
--- src/app/pages/tabs/tabs-routing.module.ts
+++ src/app/pages/tabs/tabs-routing.module.ts
@@ -6,8 +6,32 @@ import { TabsPage } from './tabs.page';
 const routes: Routes = [
   {
     path: '',
-    component: TabsPage
-  }
+    component: TabsPage,
+    children: [
+      {
+      path: 'music-list',
+      loadChildren: () => import('../music-list/music-list.module').then( m => m.MusicListPageModule)
+      },
+      {
+        path: 'music-details/:id',
+        loadChildren: () => import('../music-details/music-details.module').then( m => m.MusicDetailsPageModule)
+      },
+      {
+        path: 'profile',
+        loadChildren: () => import('../profile/profile.module').then( m => m.ProfilePageModule)
+      },
+      {
+
+        path:"",
+
+        redirectTo:"music-list",
+
+        pathMatch:"full"
+
+       }
+    ],
+    
+  },
 ];
 
 @NgModule({
