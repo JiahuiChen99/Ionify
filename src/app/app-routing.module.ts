4b77939d78080176b86f27070d1256c1da7855eb
Jiahu <Jiahui1@hotmail.es>
2020-02-17 11:51:22 +0100

    chore: Small update

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -7,11 +7,6 @@ const routes: Routes = [
     path: 'tabs',
     loadChildren: () => import('./pages/tabs/tabs.module').then( m => m.TabsPageModule)
   },
-  {
-    path: 'music-details/:id',
-    loadChildren: () => import('./pages/music-details/music-details.module').then( m => m.MusicDetailsPageModule)
-  },
-  
 ];
 
 @NgModule({
