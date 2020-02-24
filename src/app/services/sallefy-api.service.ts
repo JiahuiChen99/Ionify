2142bd85462acf3296a92e483ff137c44507fe45
Jiahu <Jiahui1@hotmail.es>
2020-02-24 16:01:14 +0100

    chore: Minor changes

--- src/app/services/sallefy-api.service.ts
+++ src/app/services/sallefy-api.service.ts
@@ -12,7 +12,7 @@ export class SallefyAPIService {
   url = 'http://sallefy-pre.eu-west-3.elasticbeanstalk.com/api/';
   //http://sallefy-pre.eu-west-3.elasticbeanstalk.com/api/
   // tslint:disable-next-line: max-line-length
-  apiKey = 'eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJhZG1pbiIsImF1dGgiOiJST0xFX0FETUlOLFJPTEVfVVNFUiIsImV4cCI6MTU4MjQ1MzQ4MH0.RGJ9OCS-BiTFIv0XrBVTdLKt53H7Trs9mYZA3deyca8sOmFDF_q3wwBYVwmx84iE-Szr6FJ-uQtRmo9n4oT4fQ';
+  apiKey = 'eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJhZG1pbiIsImF1dGgiOiJST0xFX0FETUlOLFJPTEVfVVNFUiIsImV4cCI6MTU4MjYxNzEyMH0.5aRaYnJQVzbCVB0Zlz2kj7SlOitbgeo4VFMszb2HLEi5lh980THjQ8ctq2vMvHfn3WAtMTFx3PlULQ1OF0tSLw';
   
   
 
@@ -33,17 +33,7 @@ export class SallefyAPIService {
     return this.http.get(this.url + 'search?keyword=' + songName, this.httpOptions);
   }
 
-
-  /*retrieveTracks(): Observable<any> {
-
-    let call = this.nativehttp.get( this.url + 'tracks', {}, {Authorization: 'Bearer ' + this.apiKey});
-
-    return from(call);
+  retrieveLikedTracks(): Observable<any>{
+    return this.http.get(this.url + 'me/tracks/liked', this.httpOptions);
   }
-
-  retrieveSpecificTrack(songName: String): Observable<any>{
-    let call = this.nativehttp.get(this.url + 'search?keyword=' + songName, {}, {Authorization: 'Bearer ' + this.apiKey});
-    
-    return from(call);
-  }*/
 }
