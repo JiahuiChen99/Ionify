1542a1db61544dc4cfd191c1ae39230ca7d6e4ca
Jiahu <Jiahui1@hotmail.es>
2020-02-25 08:32:46 +0100

    feat: Aunthentication guard

---
+++ src/app/guards/auth.guard.ts
@@ -0,0 +1,18 @@
+import { Injectable } from '@angular/core';
+import { ActivatedRouteSnapshot, RouterStateSnapshot, UrlTree, CanActivate } from '@angular/router';
+import { Observable } from 'rxjs';
+import { SallefyAPIService } from '../services/sallefy-api.service';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthGuard implements  CanActivate{
+  constructor(public auth: SallefyAPIService){
+  
+  }
+
+  canActivate(): boolean{
+    return this.auth.isAuthenticated();
+  }
+  
+}
