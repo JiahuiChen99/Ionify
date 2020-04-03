42fa5f62f4e92bcc84abd190adb0cca2230d5cd2
MasterofDragons <Jiahui1@hotmail.es>
2020-04-03 15:32:38 +0200

    feat: Code cleaning

--- src/app/component/more-track/more-track.component.ts
+++ src/app/component/more-track/more-track.component.ts
@@ -24,7 +24,7 @@ export class MoreTrackComponent implements OnInit {
               private popoverController: PopoverController) { }
 
   ngOnInit() {
-    console.log(this.songId);
+    //console.log(this.songId);
 
     this.service.retrieveSpecificTrack(this.songId).subscribe(result => {
       this.information = result;
@@ -87,70 +87,4 @@ export class MoreTrackComponent implements OnInit {
     });
     toast.present();
   }
-
-  /*async chromecast(track : string){
-    var appId = chrome.cast.media.DEFAULT_MEDIA_RECEIVER_APP_ID;
-    var apiConfig = new chrome.cast.ApiConfig(new chrome.cast.SessionRequest(appId), function sessionListener (session) {
-      // The session listener is only called under the following conditions:
-      // * will be called shortly chrome.cast.initialize is run
-      // * if the device is already connected to a cast session
-      // Basically, this is what allows you to re-use the same cast session 
-      // across different pages and after app restarts
-    }, function receiverListener (receiverAvailable) {
-      // receiverAvailable is a boolean.
-      // True = at least one chromecast device is available
-      // False = No chromecast devices available
-      // You can use this to determine if you want to show your chromecast icon
-    });
-    
-
-    // initialize chromecast, this must be done before using other chromecast features
-    
-    var mediaInfo = new chrome.cast.media.MediaInfo(this.trackURL, 'audio/mpeg');
-    mediaInfo.metadata = new chrome.cast.media.MusicTrackMediaMetadata();
-    //mediaInfo.metadata.metadataType = ;
-    //mediaInfo.contentType = 'audio/mpeg';
-    mediaInfo.metadata.songName = 'Ionify';
-    mediaInfo.metadata.title = this.trackInfo.name;
-    mediaInfo.metadata.artist = this.trackInfo.owner.login;
-    mediaInfo.metadata.releaseDate = '2014-02-10';
-    mediaInfo.metadata.trackNumber = '1';
-    mediaInfo.metadata.images = [{'url': this.trackInfo.thumbnail}]
-    chrome.cast.initialize(apiConfig, function () {
-            // Initialize complete
-            // Let's start casting
-            requestSession();
-        }, function (err) {
-            // Initialize failure
-        });
-
-        function requestSession () {
-          // This will open a native dialog that will let 
-          // the user choose a chromecast to connect to
-          // (Or will let you disconnect if you are already connected)
-      
-          
-          chrome.cast.requestSession(function (session) {
-              // Got a session!
-              _session = session;
-      
-              // Load a video            
-              loadMedia();
-          }, function (err) {
-              // Failed, or if err is cancel, the dialog closed
-          });
-        }
-
-
-        function loadMedia () {
-          _session.loadMedia(new chrome.cast.media.LoadRequest(mediaInfo), function (media) {
-              // You should see the video playing now!
-              // Got media!
-              _media = media;
-
-          }, function (err) {
-              // Failed (check that the video works in your browser)
-          });
-        }
-  }*/
 }
